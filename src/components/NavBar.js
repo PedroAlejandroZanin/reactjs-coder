6241c88f7a9b2ede7af76082202e5f932287fbf0
Pedro Alejandro <pedroalejandrozanin@gmail.com>
2022-01-12 20:02:19 -0300

    navbar estilos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,21 @@
-function Navbar () {
-    return <nav>Tienda ReactJS</nav>
+import "./NavBar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="navbar">
+        <li>
+          <a href="">Tienda ReactJS</a>
+        </li>
+        <li>
+          <a href="">Ofertas ReactJS</a>
+        </li>
+        <li>
+          <a href="">Contacto ReactJS</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
